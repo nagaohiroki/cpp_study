aa092fa71d31d84df6bdb8e71b0a1fa7909cf9ee
nagaohiroki <testplayers0226@gmail.com>
2018-08-19 17:34:40 +0900

    pointer test

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -1,12 +1,14 @@
 #include <memory>
 #include <stdint.h>
 #include <iostream>
-#include <type_traits>
 #include "functest.h"
 int main(int argc, char *argv[])
 {
-	std::weak_ptr<functest> c(std::nullptr_t);
-	std::shared_ptr<functest> a(new functest(1));
+	std::shared_ptr<functest> c;
+	{
+		std::shared_ptr<functest> a(new functest(1));
+		c = a;
+	}
 	std::shared_ptr<functest> b(new functest(2));
 	return 0;
 }
