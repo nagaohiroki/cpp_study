f5b049d99225ee68f02c9737347f9d43c5f7badd
nagaohiroki <testplayers0226@gmail.com>
2018-08-26 23:18:49 +0900

    function test

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -9,7 +9,9 @@ int main(int argc, char *argv[])
 	std::shared_ptr<Functest> a(new Functest(1));
 	c = a;
 	std::shared_ptr<Functest> b(new Functest(2));
+	b->Run(16);
+	b->Run(3);
 	std::cout << b.unique() << std::endl;
-	Argcheck::Test(0.5f, 1, 2, "aa", 3, 0.5f);
+	Argcheck::Test(0.5f, 1, 2, "aa", 3, 0.5f, "縺ゅ≠");
 	return 0;
 }
